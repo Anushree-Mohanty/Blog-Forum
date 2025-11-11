32e4ed02a92b42dc65e33a8de9790c2e568d1e9d
Anushree-Mohanty <anushreemohanty938@gmail.com>
2025-11-11 21:28:18 +0530

    changes made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -121,7 +121,7 @@ function Navbar({ user, handleLogout }) {
           ) : (
             <Link
               to="/signin"
-              className=" w-[200px] text-center bg-purple-600 text-black px-4 py-2 rounded-lg hover:bg-purple-700 transition-all"
+              className=" w-[200px] text-center bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-700 transition-all"
               onClick={() => setIsMenuOpen(false)} // Close menu on click
             >
               Sign In
